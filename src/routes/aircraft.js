128914e02447141ae7b56e3ce1faf5ad28f1c35b
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-01-07 21:13:19 +0100

    Update  aircraft and passenger models

--- src/routes/aircraft.js
+++ src/routes/aircraft.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const aircraftController = require('../controllers/aircraftController');
 
 router.get('/', aircraftController.getAllAircrafts);
-router.get('/:id/pilots', aircraftController.getAircraftAndPilots);
 router.get('/:id', aircraftController.getAircraft);
+router.get('/:id/pilots', aircraftController.getAircraftAndPilots);
 router.post('/', aircraftController.addAircraft);
 router.patch('/:id', aircraftController.updateAircraft);
 router.delete('/:id', aircraftController.deleteAircraft);
