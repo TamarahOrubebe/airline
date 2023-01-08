10ff0cd43fee416a9a3b7ea818fc9927f2cbf403
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-01-08 16:59:59 +0100

    Update models, routes, controllers, schema.sql, insert.sql and README

--- README.md
+++ README.md
@@ -1,1 +1,105 @@
-# airline
+This Simple Airline Reservation system was developed as the semester project of  [put course and semester detai].
+
+- You can visit the demo site from [HERE](https://airline-production.up.railway.app/)
+
+
+## Setup Guide on your local device
+
+### Database setup
+
+#### Windows
+
+Install [mysql](https://www.mysql.com/) in the local machine and setup correctly. Use following command to login to the `mysql` shell as mysql user.Use the following command on terminal
+
+```bash
+mysql -U root -p
+```
+ Then enter the root user password you used during the installation.
+
+ Then enter below commands.
+
+```sql
+CREATE USER 'godwin'@'localhost' IDENTIFIED BY 'GodwinIdeho2#';
+GRANT ALL PRIVILEGES ON * . * TO 'godwin'@'localhost';
+FLUSH PRIVILEGES;
+\q
+```
+
+Then login to `mysql` as `godwin`.
+
+```bash
+mysql -U godwin -p
+```
+Then enter the password `GodwinIdeho2#`.
+
+Next create the database `airline` and switch to it.
+
+```sql
+CREATE DATABASE airline;
+USE airline;
+\q
+```
+
+Download `database` directory from this repo and then in the shell,
+import the current DDL and DML schema. Here give the full path to the schema
+
+```sql
+\i 'C:/Users/.../database/schema.sql'
+\q
+```
+
+Check MySQL workbench after using the user godwin and password for the user to connect to the mysql server
+
+Then click on the schemas button at the bottom left of the mysql workbench after a successful connection has been created and you should see 
+a list of the databases on the server. Click on the database`airline`. Then expand it go to Tables to see the list of the tables in the database.
+
+Once that is done and working accordingly you can also get the INSERT queries from the database directory to populate your tables with dummy data.
+
+Now you are done with the database setup.
+
+
+
+### Node.js setup
+
+First clone this project directory.
+
+```bash
+git clone [your github project link]
+```
+
+Install
+
+* [node.js](https://nodejs.org/en/)
+* [npm](https://www.npmjs.com/get-npm)
+* [nodemon](https://www.npmjs.com/package/nodemon)
+
+
+
+ After that go to the project directory and run `npm install`.
+
+```bash
+cd directory/project
+npm install
+```
+
+Then create a `.env` file in the root with following content.
+You may change database user/password/secret as you may wish.
+
+```text
+DATABASE_URL=postgres://database_app:password@localhost:5432/airline_reservation_db
+
+INSTANCE_HOST='localhost'
+DB_PORT='3306'
+DB_NAME='airplane'
+DB_USER='godwin'
+DB_PASS='GodwinIdeho2#'
+```
+
+Then use `nodemon` or `node` to serve the pages.
+
+```bash
+nodemon start # If nodemon is installed
+node index.js # otherwise
+```
+
+Now visit <http://localhost:3000/> and confirm that site is running.
--- database/schema.sql
+++ database/schema.sql
@@ -1,89 +1,85 @@
--- CREATE DATABASE
-CREATE DATABASE airline;
-
--- SWITCH TO NEWLY CREATED DATABASE
-USE DATABASE airline;
-
 --  CREATION OF TABLES
 
 CREATE TABLE staff (
-id INT AUTO_INCREMENT,
-   surname VARCHAR(50),
-   given_name VARCHAR(50),
-   address VARCHAR(250),
-   telephone VARCHAR(50),
-   salary DECIMAL,
-   category VARCHAR(50),
-   PRIMARY KEY(id)
+    id INT AUTO_INCREMENT,
+    surname VARCHAR(50),
+    given_name VARCHAR(50),
+    address VARCHAR(250),
+    telephone VARCHAR(50),
+    salary DECIMAL,
+    category VARCHAR(50),
+    PRIMARY KEY(id)
    );
 
 CREATE TABLE passenger (
-id INT AUTO_INCREMENT,
-   surname VARCHAR(50),
-   given_name VARCHAR(50),
-   address VARCHAR(250),
-   telephone VARCHAR(50),
-   PRIMARY KEY(id)
+    id INT AUTO_INCREMENT,
+    surname VARCHAR(50),
+    given_name VARCHAR(50),
+    address VARCHAR(250),
+    telephone VARCHAR(50),
+    PRIMARY KEY(id)
    );
 
 CREATE TABLE airplane (
-id INT AUTO_INCREMENT,
-   manufacturer VARCHAR(50),
-   model_name VARCHAR(50),
-   model_number VARCHAR(50),
-   numnber_of_seats INT,
-   PRIMARY KEY(id)
+    id INT AUTO_INCREMENT,
+    manufacturer VARCHAR(50),
+    model_name VARCHAR(50),
+    model_number VARCHAR(50),
+    numnber_of_seats INT,
+    PRIMARY KEY(id)
    );
 
 
    CREATE TABLE rating (
-id INT AUTO_INCREMENT,
-   rating TINYINT,
-   PRIMARY KEY(id)
+    id INT AUTO_INCREMENT,
+    rating TINYINT,
+    PRIMARY KEY(id)
    );
 
    CREATE TABLE pilot(
-id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
 	staff_id INT,
 	rating_id INT,
     airplane_id INT,
-   PRIMARY KEY(id),
-   FOREIGN KEY(staff_id) REFERENCES staff(id),
-   FOREIGN KEY(rating_id) REFERENCES rating(id),
-   FOREIGN KEY(airplane_id) REFERENCES airplane(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(staff_id) REFERENCES staff(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(rating_id) REFERENCES rating(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(airplane_id) REFERENCES airplane(id) ON DELETE CASCADE ON UPDATE CASCADE
    );
 
+    CREATE TABLE flight(
+        id INT AUTO_INCREMENT,
+        origin VARCHAR(70) NOT NULL,
+        destination VARCHAR(70) NOT NULL,
+        flight_date DATE NOT NULL,
+        staff_id INT,
+        airplane_id INT,
+        passenger_id INT,
+        pilot_id INT,
+        arrival_time TIME NOT NULL,
+        departure_time TIME NOT NULL,
+        PRIMARY KEY(id),
+        FOREIGN KEY(staff_id) REFERENCES staff(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY(airplane_id) REFERENCES airplane(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY(passenger_id) REFERENCES passenger(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY(pilot_id) REFERENCES pilot(id) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
    CREATE TABLE city(
-id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
 	city_name VARCHAR(70),
     flight_id INT,
 	arrival_time TIME NOT NULL,
     departure_time TIME NOT NULL,
-   PRIMARY KEY(id)
-   FOREIGN KEY(flight_id) REFERENCES flight(id)  ON DELETE CASCADE ON UPDATE CASCADE
+    PRIMARY KEY(id)
+    FOREIGN KEY(flight_id) REFERENCES flight(id)  ON DELETE CASCADE ON UPDATE CASCADE
    );
 
-   CREATE TABLE flight(
-id INT AUTO_INCREMENT,
-	origin VARCHAR(70) NOT NULL,
-    destination VARCHAR(70) NOT NULL,
-    flight_date DATE NOT NULL,
-    staff_id INT,
-    airplane_id INT,
-    passenger_id INT,
-    pilot_id INT,
-    arrival_time TIME NOT NULL,
-    departure_time TIME NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(staff_id) REFERENCES staff(id) ON DELETE CASCADE ON UPDATE CASCADE,
-   FOREIGN KEY(airplane_id) REFERENCES airplane(id) ON DELETE CASCADE ON UPDATE CASCADE,
-   FOREIGN KEY(passenger_id) REFERENCES passenger(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(pilot_id) REFERENCES pilot(id) ON DELETE CASCADE ON UPDATE CASCADE,
-   );
+   
 
 
    CREATE TABLE booking(
-id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
 	surname VARCHAR(50),
     given_name VARCHAR(50),
     address VARCHAR(50),
@@ -94,5 +90,5 @@ id INT AUTO_INCREMENT,
     booking_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     seat_count INT,
     price DECIMAL NOT NULL,
-   PRIMARY KEY(id)
+    PRIMARY KEY(id)
    );
