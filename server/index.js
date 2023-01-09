59b50deb4845b4fe9a331b0c1f01afdca970743f
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-01-09 09:56:27 +0100

    edit package.json scripts

--- server/index.js
+++ server/index.js
@@ -26,7 +26,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, './', 'public')))
 
 //Enable cors for all routes
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 // get home page
 app.get('/*', (req, res) => {
